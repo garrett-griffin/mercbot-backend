c803eecd5a911b7688f5a6599a18481716084bf9
Garrett Griffin-Morales <grgisme@gmail.com>
2024-06-13 13:29:08 -0400

    Adding most of the necessary models, get operations only for now.

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: process.env.NODE_ENV === 'production' ? {
+            require: true,
+            rejectUnauthorized: false
+        } : false
+    }
 });
 
 module.exports = sequelize;
